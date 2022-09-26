c3801315f855cb5a7735b658d3d0900f6e5e1937
mmslotin <balu54@yandex.ru>
2022-09-26 19:33:27 +0300

    New Homework

--- src/test/java/com/demoqa/pages/RegistrationFormPage.java
+++ src/test/java/com/demoqa/pages/RegistrationFormPage.java
@@ -1,2 +1,117 @@
-package com.demoqa.pages;public class RegistrationFormPage {
+package com.demoqa.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import com.demoqa.pages.components.AddressComponent;
+import com.demoqa.pages.components.CalendarComponent;
+import com.demoqa.pages.components.ResultTableComponent;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationFormPage {
+
+    private final static String TITLE_TEXT = "Student Registration Form";
+    private CalendarComponent calendarComponent = new CalendarComponent();
+    private ResultTableComponent resultsModal = new ResultTableComponent();
+
+    private AddressComponent adressComponent = new AddressComponent();
+    private SelenideElement
+            firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            emailInput = $("#userEmail");
+
+    public RegistrationFormPage openPage() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text(TITLE_TEXT));
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        return this;
+    }
+
+    public RegistrationFormPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationFormPage clearFirstName() {
+        firstNameInput.clear();
+
+        return this;
+    }
+
+    public RegistrationFormPage setLastName(String value) {
+        lastNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationFormPage setEmail(String value) {
+        emailInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationFormPage setGender(String value) {
+        $("#genterWrapper").$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationFormPage setNumber(String value) {
+        $("#userNumber").setValue(value);
+
+        return this;
+    }
+
+    public RegistrationFormPage setBirthDate(String day, String month, String year) {
+        $("#dateOfBirthInput").click();
+        calendarComponent.setDate(day, month, year);
+
+        return this;
+    }
+
+    public RegistrationFormPage setSubjects(String value) {
+        $("#subjectsInput").setValue(value).pressEnter();
+
+        return this;
+    }
+
+    public RegistrationFormPage setHobbies(String value) {
+        $("#hobbiesWrapper").$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationFormPage setPicture(String value) {
+        $("#uploadPicture").uploadFromClasspath(value);
+
+        return this;
+    }
+
+    public RegistrationFormPage setAddress(String address, String state, String city) {
+        adressComponent.setDate(address, state, city);
+
+        return this;
+    }
+
+    public RegistrationFormPage clickSubmit () {
+        $("#submit").click();
+
+        return this;
+    }
+
+    public RegistrationFormPage checkResultsTableVisible() {
+        resultsModal.checkVisible();
+
+        return this;
+    }
+
+    public RegistrationFormPage checkResult(String key, String value) {
+        resultsModal.checkResult(key, value);
+
+        return this;
+    }
 }
--- src/test/java/com/demoqa/test/TextBoxTestWithPageObjectsTest.java
+++ src/test/java/com/demoqa/test/TextBoxTestWithPageObjectsTest.java
@@ -1,2 +1,53 @@
-package com.demoqa.test;public class TextBoxTestWithPageObjectsTest {
+package com.demoqa.tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.demoqa.pages.RegistrationFormPage;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.by;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxTestWithPageObjectsTest {
+
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillFormTest() {
+        registrationFormPage.openPage()
+                .setFirstName("Miha")
+                .setLastName("Mishgun")
+                .setEmail("mish@gun.com")
+                .setGender("Male")
+                .setNumber("9999888777")
+                .setBirthDate("17", "July", "1988")
+                .setSubjects("Computer Science")
+                .setHobbies("Sports")
+                .setPicture("12345.png")
+                .setAddress("Calgary", "NCR", "Delhi")
+                .clickSubmit();
+
+        registrationFormPage.checkResultsTableVisible()
+                .checkResult("Student Name", "Miha Mishgun")
+                .checkResult("Student Email", "mish@gun.com")
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", "9999888777")
+                .checkResult("Date of Birth", "17 July,1988")
+                .checkResult("Subjects", "Computer Science")
+                .checkResult("Hobbies", "Sports")
+                .checkResult("Picture", "12345.png")
+                .checkResult("Address", "Calgary")
+                .checkResult("State and City", "NCR Delhi");
+
+    }
+
 }
